fef2aad99deabb035b6f6591a15748bac7bc4bcb
nultero <nultero@protonmail.com>
2022-08-04 22:25:01 -0500

    Remove hertz

--- state/cpu_t.go
+++ state/cpu_t.go
@@ -2,13 +2,10 @@ package state
 
 import (
 	"fmt"
-	"os/exec"
-	"strconv"
 )
 
 type cpu_t struct {
-	LCI     int     // last CPU idle %, used for PollCPU
-	HZ      float64 // clock tic hertz, originally a double in procps, used here for conversions to jiffies
+	LCI     int // last CPU idle %, used for PollCPU
 	Stamps  []float64
 	Sum     int // CPU jiffies currently in play
 	SumPrev int // CPU jiffies previous
@@ -17,7 +14,6 @@ type cpu_t struct {
 func defaultCpu_t() cpu_t {
 	return cpu_t{
 		LCI:     0,
-		HZ:      getHertz(),
 		Stamps:  []float64{},
 		Sum:     0,
 		SumPrev: 0,
@@ -39,39 +35,4 @@ func (c cpu_t) LastToStr() string {
 	}
 
 	return fmt.Sprintf("%.2f", pc)
-}
-
-func getHertz() float64 {
-
-	/*
-	 this honestly seemed like
-	 the simplest way to get this info;
-	 there is exposed HZ figures in
-	 /proc/cpuinfo but they can vary,
-	 apparently, and so here is the
-	 dumber variant
-	*/
-
-	c := exec.Command("getconf", "CLK_TCK")
-	b, err := c.Output()
-	if err != nil {
-		e := fmt.Errorf("err interpreting `getconf` output for clock ticks: %w", err)
-		panic(e)
-	}
-
-	bytes := []byte{}
-	for _, ch := range b {
-		if ch != '\n' {
-			bytes = append(bytes, ch)
-		}
-	}
-
-	s := string(bytes)
-	hz, err := strconv.ParseFloat(s, 64)
-	if err != nil {
-		e := fmt.Errorf("err interpreting `getconf` string for clock ticks: %w", err)
-		panic(e)
-	}
-
-	return hz
 }
